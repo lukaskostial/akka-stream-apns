a1e384e28c319d48c7dc6d5609e5ea8379cc5916
Marcel Mojzis <marcel.mojzis@reactivehub.com>
2016-04-05 09:12:23 +0200

    Avoid dead code elimination in the DeviceToken benchmark

--- benchmarks/src/main/scala/reactivehub/akka/stream/apns/DeviceTokenBenchmark.scala
+++ benchmarks/src/main/scala/reactivehub/akka/stream/apns/DeviceTokenBenchmark.scala
@@ -4,23 +4,24 @@ import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations._
 import reactivehub.akka.stream.apns.DeviceTokenBenchmark._
 
+object DeviceTokenBenchmark {
+  val Digits = "0123456789ABCDEF"
+
+  val tokenBytes = List[Byte](1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32)
+}
+
 @BenchmarkMode(Array(Mode.Throughput))
 @Warmup(time = 10, timeUnit = TimeUnit.SECONDS)
 @Measurement(time = 10, timeUnit = TimeUnit.SECONDS)
 class DeviceTokenBenchmark {
   @Benchmark
-  def testStringFormat(): Unit = TokenBytes.map("%02X" format _).mkString
+  def testStringFormat: String =
+    tokenBytes.map("%02X" format _).mkString
 
   @Benchmark
-  def testBuilder(): Unit =
-    TokenBytes.foldLeft(new StringBuilder) {
+  def testBuilder: String =
+    tokenBytes.foldLeft(new StringBuilder) {
       case (sb, b) ⇒ sb.append(Digits((b & 0xF0) >> 4)).append(Digits(b & 0x0F))
     }.toString
-}
-
-object DeviceTokenBenchmark {
-  val Digits = "0123456789ABCDEF"
-
-  val TokenBytes = List[Byte](1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32)
 }
